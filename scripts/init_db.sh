1e2197d87fd94840c043bef14da2ad68283e376e
Sarakuzoi <calin.andrei.radoi@gmail.com>
2023-07-14 13:07:35 +0300

    Added first migration

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -29,26 +29,26 @@ DB_HOST="${POSTGRES_HOST:=localhost}"
 # Allow to skip Docker if a dockerized Postgres database is already running
 if [[ -z "${SKIP_DOCKER}" ]]
 then
-  # if a postgres container is running, print instructions to kill it and exit
-  RUNNING_POSTGRES_CONTAINER=$(docker ps --filter 'name=postgres' --format '{{.ID}}')
-  if [[ -n $RUNNING_POSTGRES_CONTAINER ]]; then
-    echo >&2 "there is a postgres container already running, kill it with"
-    echo >&2 "    docker kill ${RUNNING_POSTGRES_CONTAINER}"
-    exit 1
-  fi
+#   # if a postgres container is running, print instructions to kill it and exit
+#   RUNNING_POSTGRES_CONTAINER=$(docker ps --filter 'name=postgres' --format '{{.ID}}')
+#   if [[ -n $RUNNING_POSTGRES_CONTAINER ]]; then
+#     echo >&2 "there is a postgres container already running, kill it with"
+#     echo >&2 "    docker kill ${RUNNING_POSTGRES_CONTAINER}"
+#     exit 1
+#   fi
   # Launch postgres using Docker
   docker run \
       -e POSTGRES_USER=${DB_USER} \
       -e POSTGRES_PASSWORD=${DB_PASSWORD} \
       -e POSTGRES_DB=${DB_NAME} \
       -p "${DB_PORT}":5432 \
-      -d \
-      --name "postgres_$(date '+%s')" \
+      -d postgres \
       postgres -N 1000
       # ^ Increased maximum number of connections for testing purposes
 fi
 
 # Keep pinging Postgres until it's ready to accept commands
+export PGPASSWORD="${DB_PASSWORD}"
 until PGPASSWORD="${DB_PASSWORD}" psql -h "${DB_HOST}" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
   >&2 echo "Postgres is still unavailable - sleeping"
   sleep 1
