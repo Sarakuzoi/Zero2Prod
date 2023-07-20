33ac3ae59e10fc5af430b02dd40a7d9f9ff63f00
Sarakuzoi <calin.andrei.radoi@gmail.com>
2023-07-20 16:13:22 +0300

    Added sqlx offline, finished Dockerfile
    Sqlx calls into our db at compile time to ensure that all queries can be successfully executed, considering the schemas of our tables. When running `cargo run` inside our Docker image, sqlx fails to establish a connection with the db specified in our .env file.
    With `sqlx prepare` we can save metadata for all invocations of `query!` and related macros to `sqlx-data.json`, so sqlx can perform its compile-time checks

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,11 @@ FROM rust:1.71.0
 
 # Basically cd /app, and if it doesn't exist, Docker creates the folder
 WORKDIR /app
-RUN apt update && apt install clang -y
+RUN apt update && apt install lld clang -y
 
 # Copy all files from our working environment to our Docker image
 COPY . .
+ENV SQLX_OFFLINE true
 RUN cargo build --release
 # When docker run is executed, launch the binary
+ENTRYPOINT [ "./target/release/zero2prod" ]
--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,10 @@ async fn main() -> Result<(), std::io::Error> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read the configuration");
+    // No longer async, given that we no longer try to connect, for Docker
     let connection_pool =
-        PgPool::connect(configuration.database.connection_string().expose_secret())
-            .await
-            .expect("Failed to connect to Postgres");
+        PgPool::connect_lazy(configuration.database.connection_string().expose_secret())
+            .expect("Failed to create Postgres connection pool");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(&address)?;
     run(listener, connection_pool)?.await
