8a47b1e1a350c6d93851c003f0081d986b3ed16e
Sarakuzoi <calin.andrei.radoi@gmail.com>
2023-07-27 23:06:37 +0300

    Added  to the rest of the project. Fixed tests

--- src/domain/new_subscriber.rs
+++ src/domain/new_subscriber.rs
@@ -1,6 +1,7 @@
+use super::SubscriberEmail;
 use crate::domain::subscriber_name::SubscriberName;
 
 pub struct NewSubscriber {
-    pub email: String,
+    pub email: SubscriberEmail,
     pub name: SubscriberName,
 }
