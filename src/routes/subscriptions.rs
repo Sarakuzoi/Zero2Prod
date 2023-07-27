0b1bbebfdfbb829d45910f75b7964eddf519a84b
Sarakuzoi <calin.andrei.radoi@gmail.com>
2023-07-27 21:34:15 +0300

    Added tests, handling the Result returned by the parser

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,7 +1,6 @@
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
-use unicode_segmentation::UnicodeSegmentation;
 use uuid::Uuid;
 
 use crate::domain::{NewSubscriber, SubscriberName};
@@ -21,26 +20,18 @@ pub struct FormData {
     )
 )]
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    let name = match SubscriberName::parse(form.0.name) {
+        Ok(name) => name,
+        Err(_) => return HttpResponse::BadRequest().finish(),
+    };
     let new_subscriber = NewSubscriber {
         email: form.0.email,
-        name: SubscriberName::parse(form.0.name).expect("Name validation failed"),
+        name: name,
     };
     match insert_subscriber(&pool, &new_subscriber).await {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(_) => HttpResponse::InternalServerError().finish(),
     }
-}
-
-pub fn is_valid_name(s: &str) -> bool {
-    let is_empty_or_whitespace = s.trim().is_empty();
-
-    let is_too_long = s.graphemes(true).count() > 256;
-
-    let forbidden_characters = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
-    // let contains_forbidden_characters = s.contains(forbidden_characters);
-    let contains_forbidden_characters = s.chars().any(|g| forbidden_characters.contains(&g));
-
-    !(is_empty_or_whitespace || is_too_long || contains_forbidden_characters)
 }
 
 #[tracing::instrument(
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -138,34 +138,34 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
     }
 }
 
-// #[tokio::test]
-// async fn subscribe_returns_a_400_when_fields_are_present_but_empty() {
-//     let test_app = spawn_app().await;
-//     let client = reqwest::Client::new();
-//     let test_cases = vec![
-//         ("name=&email=karaiozus%40gmail.com", "empty name"),
-//         ("name=kara%20iozus&email=", "empty email"),
-//         (
-//             "name=Karaiozus&email=invalid-email-address",
-//             "invalid email",
-//         ),
-//     ];
+#[tokio::test]
+async fn subscribe_returns_a_400_when_fields_are_present_but_empty() {
+    let test_app = spawn_app().await;
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=&email=karaiozus%40gmail.com", "empty name"),
+        ("name=kara%20iozus&email=", "empty email"),
+        (
+            "name=Karaiozus&email=invalid-email-address",
+            "invalid email",
+        ),
+    ];
 
-//     for (invalid_body, description) in test_cases {
-//         let response = client
-//             .post(&format!("{}/subscriptions", &test_app.address))
-//             .header("Content-Type", "application/x-www-form-urlencoded")
-//             .body(invalid_body)
-//             .send()
-//             .await
-//             .expect("Failed to execute request.");
+    for (invalid_body, description) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &test_app.address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
 
-//         assert_eq!(
-//             400,
-//             response.status().as_u16(),
-//             // Additional customised error message on test failure
-//             "The API did not return a 400 Bad Request when the payload was {}",
-//             description
-//         )
-//     }
-// }
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            // Additional customised error message on test failure
+            "The API did not return a 400 Bad Request when the payload was {}",
+            description
+        )
+    }
+}
