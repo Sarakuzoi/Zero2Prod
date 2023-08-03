1d77cae3e14b1ced89b47f0404ff64b5c81a8c6d
Sarakuzoi <calin.andrei.radoi@gmail.com>
2023-08-03 20:23:20 +0300

    Added migrations for new 'status' column in subscriptions table. Added new table subscription_tokens

---
+++ migrations/20230803171853_create_subscription_tokens_table.sql
@@ -0,0 +1,7 @@
+-- Create subscription tokens table
+CREATE TABLE subscription_tokens (
+    subscription_tokens TEXT NOT NULL,
+    subscriber_id uuid NOT NULL
+        REFERENCES subscriptions (id),
+    PRIMARY KEY (subscription_tokens)
+)
