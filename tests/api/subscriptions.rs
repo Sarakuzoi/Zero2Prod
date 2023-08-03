815a3d24e7124f8edaea32b867c1c842271fd6f0
Sarakuzoi <calin.andrei.radoi@gmail.com>
2023-08-03 21:30:46 +0300

    Added green testing for email being sent

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -1,3 +1,8 @@
+use wiremock::{
+    matchers::{method, path},
+    Mock, ResponseTemplate,
+};
+
 use crate::helpers::spawn_app;
 
 #[tokio::test]
@@ -5,6 +10,12 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     // Arrange
     let test_app = spawn_app().await;
     let body = "name=sara%20kuzoi&email=sara_kuzoi%40gmail.com";
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&test_app.email_server)
+        .await;
 
     // Act
     let response = test_app.post_subscriptions(body.into()).await;
@@ -66,4 +77,22 @@ async fn subscribe_returns_a_400_when_fields_are_present_but_empty() {
             description
         )
     }
+}
+
+#[tokio::test]
+async fn subscribe_sends_a_confirmation_email_for_valid_data() {
+    // Arrange
+    let app = spawn_app().await;
+    let body = "name=sara%20kuzoi&email=sara_kuzoi%40tuta.io";
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        // Assert, is checked at the end as the MockServer is shutting down
+        .expect(1)
+        .mount(&app.email_server)
+        .await;
+
+    // Act
+    app.post_subscriptions(body.into()).await;
 }
