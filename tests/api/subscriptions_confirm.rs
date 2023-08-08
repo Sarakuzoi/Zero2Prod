05e7163d34a386694880a3ad9f37db6f9844afed
Sarakuzoi <calin.andrei.radoi@gmail.com>
2023-08-09 02:04:17 +0300

    Added test for clicking twice on the confirmation link

--- tests/api/subscriptions_confirm.rs
+++ tests/api/subscriptions_confirm.rs
@@ -77,3 +77,42 @@ async fn clicking_on_the_confirmation_link_confirms_a_subscriber() {
     assert_eq!(saved.name, "sara kuzoi");
     assert_eq!(saved.status, "confirmed");
 }
+
+#[tokio::test]
+async fn clicking_twice_on_the_confirmation_link_confirms_a_subscriber() {
+    // Arrange
+    let test_app = spawn_app().await;
+    let body = "name=sara%20kuzoi&email=sara_kuzoi%40tuta.io";
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&test_app.email_server)
+        .await;
+
+    test_app.post_subscriptions(body.into()).await;
+    let email_request = &test_app.email_server.received_requests().await.unwrap()[0];
+    let confirmation_links = test_app.get_confirmation_links(&email_request);
+
+    // Act
+    reqwest::get(confirmation_links.html)
+        .await
+        .unwrap()
+        .error_for_status()
+        .unwrap();
+    reqwest::get(confirmation_links.plain_text)
+        .await
+        .unwrap()
+        .error_for_status()
+        .unwrap();
+
+    // Assert
+    let query = sqlx::query!("SELECT email, name, status FROM subscriptions")
+        .fetch_one(&test_app.db_pool)
+        .await
+        .expect("Failed to fetch saved subscriber");
+
+    assert_eq!(query.email, "sara_kuzoi@tuta.io");
+    assert_eq!(query.name, "sara kuzoi");
+    assert_eq!(query.status, "confirmed");
+}
