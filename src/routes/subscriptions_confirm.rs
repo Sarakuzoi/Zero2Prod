9b7582763c2ae61d50e480ca488fabf3e9a9041c
Sarakuzoi <calin.andrei.radoi@gmail.com>
2023-08-16 11:48:09 +0300

    Added improved error handling for subscriptions' confirm handler

--- src/routes/subscriptions_confirm.rs
+++ src/routes/subscriptions_confirm.rs
@@ -1,10 +1,34 @@
-use actix_web::{web, HttpResponse};
+use actix_web::{web, HttpResponse, ResponseError};
+use anyhow::Context;
+use reqwest::StatusCode;
 use sqlx::PgPool;
 use unicode_segmentation::UnicodeSegmentation;
 use uuid::Uuid;
 
+#[derive(thiserror::Error)]
+pub enum ConfirmError {
+    #[error("There is no subscriber associated with the provided token")]
+    UnknownToken,
+    #[error(transparent)]
+    UnexpectedError(#[from] anyhow::Error),
+}
+
+impl std::fmt::Debug for ConfirmError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        error_chain_fmt(self, f)
+    }
+}
+
+impl ResponseError for ConfirmError {
+    fn status_code(&self) -> reqwest::StatusCode {
+        match self {
+            ConfirmError::UnknownToken => StatusCode::UNAUTHORIZED,
+            ConfirmError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+        }
+    }
+}
+
 #[derive(serde::Deserialize)]
-#[allow(dead_code)]
 pub struct Parameters {
     subscription_token: String,
 }
@@ -12,27 +36,23 @@ pub struct Parameters {
 #[tracing::instrument(name = "Confirm a pending subscriber", skip(parameters, pool))]
 // The parameter of type web::Query<> instructs actix-web to only call the handler if the extractions was successful.
 // Otherwise it returns a 400 Bad Request
-pub async fn confirm(parameters: web::Query<Parameters>, pool: web::Data<PgPool>) -> HttpResponse {
-    if !token_is_valid(&parameters.subscription_token) {
-        return HttpResponse::BadRequest().finish();
-    }
-    let id = match get_subscriber_id_from_token(&pool, &parameters.subscription_token).await {
-        Ok(id) => id,
-        Err(_) => return HttpResponse::InternalServerError().finish(),
-    };
-    match id {
-        // Non-existing token
-        None => HttpResponse::Unauthorized().finish(),
-        Some(subscriber_id) => {
-            if confirm_subscriber(&pool, subscriber_id).await.is_err() {
-                return HttpResponse::InternalServerError().finish();
-            }
-            HttpResponse::Ok().finish()
-        }
-    }
+pub async fn confirm(
+    parameters: web::Query<Parameters>,
+    pool: web::Data<PgPool>,
+) -> Result<HttpResponse, ConfirmError> {
+    // TODO: Improve Error handling
+    token_is_valid(&parameters.subscription_token)?;
+    let id = get_subscriber_id_from_token(&pool, &parameters.subscription_token)
+        .await
+        .context("Failed to get the subscriber id associated with the token")?
+        .ok_or(ConfirmError::UnknownToken)?;
+    confirm_subscriber(&pool, id)
+        .await
+        .context("Failed to update the subscriber's status to `confirmed`.")?;
+    Ok(HttpResponse::Ok().finish())
 }
 
-fn token_is_valid(s: &str) -> bool {
+fn token_is_valid(s: &str) -> Result<(), ConfirmError> {
     let is_empty_or_whitespace = s.trim().is_empty();
 
     let invalid_length = s.graphemes(true).count() != 25;
@@ -41,7 +61,11 @@ fn token_is_valid(s: &str) -> bool {
     // let contains_forbidden_characters = s.contains(forbidden_characters);
     let contains_forbidden_characters = s.chars().any(|g| forbidden_characters.contains(&g));
 
-    !(is_empty_or_whitespace || invalid_length || contains_forbidden_characters)
+    if !(is_empty_or_whitespace || invalid_length || contains_forbidden_characters) {
+        Ok(())
+    } else {
+        Err(ConfirmError::UnknownToken)
+    }
 }
 
 async fn get_subscriber_id_from_token(
@@ -73,5 +97,18 @@ pub async fn confirm_subscriber(pool: &PgPool, subscriber_id: Uuid) -> Result<()
         tracing::error!("Failed to execute query: {:?}", e);
         e
     })?;
+    Ok(())
+}
+
+fn error_chain_fmt(
+    e: &impl std::error::Error,
+    f: &mut std::fmt::Formatter<'_>,
+) -> std::fmt::Result {
+    writeln!(f, "{}\n", e)?;
+    let mut current = e.source();
+    while let Some(cause) = current {
+        writeln!(f, "Caused by:\n\t{}", cause)?;
+        current = cause.source();
+    }
     Ok(())
 }
