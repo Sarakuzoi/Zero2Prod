346b2c70466ff30d3cff257b913553e70d53b2d0
Sarakuzoi <calin.andrei.radoi@gmail.com>
2023-08-16 23:58:42 +0300

    First newsletter test

---
+++ tests/api/newsletter.rs
@@ -0,0 +1,58 @@
+use wiremock::{
+    matchers::{any, method, path},
+    Mock, ResponseTemplate,
+};
+
+use crate::helpers::{spawn_app, TestApp};
+
+#[tokio::test]
+#[ignore]
+async fn newsletters_are_not_delivered_to_unconfirmed_subscribers() {
+    // Arrange
+    let test_app = spawn_app().await;
+    create_unconfirmed_subscriber(&test_app).await;
+
+    Mock::given(any())
+        .respond_with(ResponseTemplate::new(200))
+        // We assert that no request is fired from Postmark
+        .expect(0)
+        .mount(&test_app.email_server)
+        .await;
+
+    // Act
+
+    // Initial sketch of newsletter payload structure
+    let newsletter_request_body = serde_json::json!({
+        "title": "Newsletter title",
+        "content": {
+            "text": "Newsletter body as plain text",
+            "html": "<p>Newsletter body as HTML</p>"
+        }
+    });
+    let response = reqwest::Client::new()
+        .post(&format!("{}/newsletter", &test_app.address))
+        .json(&newsletter_request_body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert_eq!(response.status().as_u16(), 200);
+}
+
+// Use the public API of the application under test to create an unconfirmed subscriber
+async fn create_unconfirmed_subscriber(app: &TestApp) {
+    let body = "name=sara%20kuzoi&email=sara_kuzoi%40tuta.io";
+
+    let _mock_guard = Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .named("Create unconfirmed subscriber")
+        .expect(1)
+        .mount_as_scoped(&app.email_server)
+        .await;
+    app.post_subscriptions(body.into())
+        .await
+        .error_for_status()
+        .unwrap();
+}
